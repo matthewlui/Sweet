12da51276aa71b0c05e3352cfa089b65e201763c
matthewlui <matthewluihk@gmail.com>
2015-12-06 16:23:39 +0800

    Move SWJSON to independence project

--- Package.swift
+++ Package.swift
@@ -1,5 +1,7 @@
 import PackageDescription
 
 let package = Package(
-name: "Sweet"
+name: "Sweet",
+dependencies : [.Package(url:"https://github.com/matthewlui/SWStringExtension.git",majorVersion:1),
+.Package(url:"https://github.com/matthewlui/SWJSON.git",majorVersion:1)]
 )
